ca63afb44a7fb4f5a3987b7059a3c69879503b3e
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-12-11 11:26:24 -0300

    Alta Cliente
    Alta de clientes vip y regulares terminado correctamente

---
+++ src/main/java/com/example/oblig/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.oblig.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class WebConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebConfig.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Ajusta a tu dominio de cliente
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
